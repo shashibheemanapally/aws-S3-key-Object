7b944a66e6600eeece366d21499970d4b751754f
Shashi <shashibheemanapally@gmail.com>
2021-09-11 22:26:42 +0530

    Initial commit

---
+++ src/test/java/com/qwert/awstest/AwstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwert.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
